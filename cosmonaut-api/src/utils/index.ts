59dc622103f4b474b336e071862c7a54d593c941
Jaeseung Lee <41176085+tkxkd0159@users.noreply.github.com>
2022-09-29 15:20:08 +0900

    fix: not allow `std` crate for system to prevent unexpected errors

--- cosmonaut-api/src/utils/index.ts
+++ cosmonaut-api/src/utils/index.ts
@@ -1,6 +1,6 @@
 import path from "path";
 import { writeFile, readFile, readdir } from "fs/promises";
-import {createHash} from 'crypto';
+import { createHash } from "crypto";
 import { NextFunction } from "express";
 import { Base64, RustFiles, expressAsyncHandler } from "@d3lab/types";
 
@@ -15,77 +15,77 @@ const b64ToStr = (raw: Base64): string => {
 };
 
 async function _saveCode(path: string, code: Base64) {
-    try {
-        const stringCode = b64ToStr(code).trim();
-        await writeFile(path, stringCode);
-    } catch (err) {
-        console.error(err);
+    const stringCode = b64ToStr(code).trim();
+    if (stringCode.match(/\b(?!cosmwasm_\b)std::\b(?!convert\b)\b(?!ops\b)/g)) {
+        throw new Error("This code is not valid.");
     }
+    await writeFile(path, stringCode);
 }
 
 async function saveCodeFiles(files: RustFiles, projPath: string) {
-    try {
-        for (let [filename, code] of Object.entries(files)) {
-            const fpath = path.join(projPath, filename);
-            await _saveCode(fpath, code);
-        }
-    } catch (err) {
-        console.error(err);
+    for (let [filename, code] of Object.entries(files)) {
+        const fpath = path.join(projPath, filename);
+        await _saveCode(fpath, code);
     }
 }
 
 async function lodeCodeFiles(projPath: string): Promise<RustFiles | undefined> {
     try {
-        const filelist = await readdir(projPath)
-        const result: RustFiles = {}
+        const filelist = await readdir(projPath);
+        const result: RustFiles = {};
         for (let fname of filelist) {
             if (fname.slice(-3) !== ".rs") {
-                continue
+                continue;
             }
-            let fpath = path.join(projPath, fname)
-            let code = await readFile(fpath)
-            result[fname] = code.toString('base64')
+            let fpath = path.join(projPath, fname);
+            let code = await readFile(fpath);
+            result[fname] = code.toString("base64");
         }
-        return result
-    } catch(err) {
-        console.error(err)
+        return result;
+    } catch (err) {
+        console.error(err);
     }
 }
 
 function makeLessonPicturePath(lesson: number): string {
-    const dirPrefix = createHash('sha256').update(`cosmonaut-lesson${lesson}`).digest('hex') + `-${lesson}`
-    const dirPath = path.join('assets', dirPrefix)
-    return dirPath
+    const dirPrefix =
+        createHash("sha256").update(`cosmonaut-lesson${lesson}`).digest("hex") +
+        `-${lesson}`;
+    const dirPath = path.join("assets", dirPrefix);
+    return dirPath;
 }
 
 function srcStrip(origin: string): string {
-    const target = '/src'
-    let cursor = 0
-    let isStart = true
+    const target = "/src";
+    let cursor = 0;
+    let isStart = true;
 
     while (true) {
-        let tmpCursor = origin.indexOf(target, isStart ? cursor : cursor + target.length)
-        isStart = false
+        let tmpCursor = origin.indexOf(
+            target,
+            isStart ? cursor : cursor + target.length
+        );
+        isStart = false;
         if (tmpCursor === -1) {
-            break
+            break;
         } else {
-            cursor = tmpCursor
+            cursor = tmpCursor;
         }
     }
 
-    if (cursor <= 0 ) {
-        return origin
+    if (cursor <= 0) {
+        return origin;
     } else {
-        return origin.slice(0, cursor)
+        return origin.slice(0, cursor);
     }
 }
 
-const asyncUtil: expressAsyncHandler = fn =>
-function asyncUtilWrap(...args) {
-  const fnReturn = fn(...args)
-  const next = args[args.length-1] as NextFunction
-  return Promise.resolve(fnReturn).catch(next)
-}
+const asyncUtil: expressAsyncHandler = (fn) =>
+    function asyncUtilWrap(...args) {
+        const fnReturn = fn(...args);
+        const next = args[args.length - 1] as NextFunction;
+        return Promise.resolve(fnReturn).catch(next);
+    };
 
 export {
     sleep,
@@ -94,5 +94,5 @@ export {
     saveCodeFiles,
     srcStrip,
     makeLessonPicturePath,
-    asyncUtil
+    asyncUtil,
 };
